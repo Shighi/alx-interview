cd5be18c6a7b6a19d1ccab72deaa27256dba6539
Shighi <daisyshighi@gmail.com>
2025-04-16 08:52:52 +0000

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,41 @@
-#!/usr/bin/env python3
-from typing import List
+#!/usr/bin/python3
+"""
+Module for Pascal's Triangle
+"""
 
 
-def pascal_triangle(n: int) -> List[list]:
-    '''
-    Pascal triangle
-    '''
+def pascal_triangle(n):
+    """
+    Function that returns a list of lists of integers representing
+    the Pascal's triangle of n.
+    
+    Args:
+        n (int): Number of rows in Pascal's Triangle
+        
+    Returns:
+        list: A list of lists containing integers representing Pascal's Triangle
+              Returns an empty list if n <= 0
+    """
     if n <= 0:
         return []
-
-    if n == 1:
-        return [[1]]
-
-    if n == 2:
-        return [[1], [1, 1]]
-
-    triangle = [[1], [1, 1]]
-
-    for i in range(2, n):
-        temp = [1, 1]
-        for j in range(0, len(triangle[-1])-1):
-            a = triangle[-1][j]
-            b = triangle[-1][j+1]
-            temp.insert(-1, a + b)
-        triangle.append(temp)
-
+    
+    triangle = [[1]]  # Initialize with the first row
+    
+    for i in range(1, n):
+        # Previous row
+        prev_row = triangle[-1]
+        # Start new row with 1
+        current_row = [1]
+        
+        # Calculate middle values for current row
+        for j in range(1, i):
+            # Each value is the sum of the two values above it
+            current_row.append(prev_row[j-1] + prev_row[j])
+        
+        # End the row with 1
+        current_row.append(1)
+        
+        # Add the current row to the triangle
+        triangle.append(current_row)
+    
     return triangle
